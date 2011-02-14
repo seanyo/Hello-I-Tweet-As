75d3739d8a94ff6ce2d7ba6f0f236628782b0959
Chris Charles <chris@ccharles.ca>
2011-02-14 11:57:00 -0500

    Issue #6: Added basic error handling. Error message put on name tag.

--- scripts/helloITweetAs.js
+++ scripts/helloITweetAs.js
@@ -26,20 +26,43 @@ function getParam(key) {
 
 
 var twitterID = getParam("tid");
-var tweeturl = "http://api.twitter.com/1/users/show/" +
-  twitterID + ".json?callback=?";
+var tweeturl = "http://api.twitter.com/1/users/show/" + twitterID + ".json";
 
 $(document).ready(function() {
-  $.getJSON(tweeturl, function(data){
-    var userPicURL = data.profile_image_url.replace("normal", "bigger");
-    var screenName = data.screen_name;
-    var name = data.name;
-    var description = data.description;
-    var location = data.location;
-    var userPic='<img width=75 height=75 src="'+userPicURL+'" />';
-    $('div.left').html(userPic);
-    $('p.twitterID').html("@"+screenName);
-    $('p.name').html(name+" "+location);
-    $('p.description').html(description);
+  // Need to suppress regular HTTP response codes since jQuery's JSONP
+  // support doesn't handle error callbacks for cross-domain requests. True
+  // as of jQuery 1.5.
+  $.getJSON(tweeturl + "?suppress_response_codes&callback=?", function(data) {
+    if (data.error === undefined) {
+      var userPicURL = data.profile_image_url.replace("normal", "bigger");
+      var screenName = data.screen_name;
+      var name = data.name;
+      var description = data.description;
+      var location = data.location;
+      var userPic='<img width=75 height=75 src="'+userPicURL+'" />';
+      $('div.left').html(userPic);
+      $('p.twitterID').html("@"+screenName);
+      $('p.name').html(name+" "+location);
+      $('p.description').html(description);
+    }
+    else {
+      // Because we had to suppress HTTP error codes (see above) we don't get
+      // numeric errors. Have to test against strings to figure out what
+      // broke.
+      switch (data.error) {
+        case "Not found":
+          $("p.twitterID").html("Error: Could not retrieve your profile.");
+          break;
+        case "Internal server error":
+          $("p.twitterID").html("Error: Twitter appears to be down.");
+          break;
+        case "Service unavailable":
+          $("p.twitterID").html("Error: Twitter is experiencing heavy load.");
+          break;
+        default:
+          $("p.twitterID").html("Error: Something went wrong.");
+          break;
+      }
+    }
   });
 });
