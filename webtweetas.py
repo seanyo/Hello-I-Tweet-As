8d242fcbe64392dc3b30c31d91c5728e37b765b7
Chris Charles <chris@ccharles.ca>
2011-06-22 12:55:54 -0400

    Basic authentication to Twitter is now working.

--- webtweetas.py
+++ webtweetas.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python2
 
+import ConfigParser, os, urlparse
+
 try:
     import web
 except ImportError, e:
@@ -7,21 +9,31 @@ except ImportError, e:
     print >> sys.stderr, "You need to install web.py"
     sys.exit(1)
 
+import oauth2 as oauth
+
 from itweetas import LabelBuilder, LabelFormat, TwitterUser
 from calibration import CalibrationPage
 
+
+# We don't want web.seeother() to include the script name in the URL
+os.environ['REAL_SCRIPT_NAME'] = ''
 
 render = web.template.render('templates/')
 
 urls = (
     '/', 'index',
     '/calibrate', 'calibrate',
+    '/login', 'login',
+    '/logout', 'logout',
     '/nametags', 'pdf'
     )
 
 class index:
     def GET(self):
-        return render.index()
+        user = ''
+        if hasattr(session, 'oauth_token'):
+            user = session.access_token['screen_name']
+        return render.index(user=user)
 
 
 class calibrate:
@@ -31,6 +43,54 @@ class calibrate:
         web.header('Content-Type', 'application/pdf')
         web.header('Content-Disposition', 'attachment; filename="calibration.pdf"')
         return format(calibrationPDF.getPDF())
+
+
+class login:
+    def GET(self):
+        i = web.input()
+
+        config = ConfigParser.ConfigParser()
+        config.read('itweetas.cfg')
+
+        consumer_key = config.get('oauth', 'consumer_key')
+        consumer_secret = config.get('oauth', 'consumer_secret')
+
+        request_token_url = 'https://twitter.com/oauth/request_token'
+        access_token_url = 'https://twitter.com/oauth/access_token'
+        authorize_url = 'https://twitter.com/oauth/authorize'
+
+        consumer = oauth.Consumer(consumer_key, consumer_secret)
+        client = oauth.Client(consumer)
+
+        if hasattr(i, 'oauth_token') and hasattr(i, 'oauth_verifier'):
+            token = oauth.Token(i.oauth_token,
+                                session.oauth_token_secret)
+            token.set_verifier(i.oauth_verifier)
+            client = oauth.Client(consumer, token)
+
+            resp, content = client.request(access_token_url, "POST")
+            session.access_token = dict(urlparse.parse_qsl(content))
+
+            raise web.seeother('/')
+
+        else:
+            resp, content = client.request(request_token_url, "GET")
+            if resp['status'] != '200':
+                raise Exception("Invalid response %s." % resp['status'])
+
+            request_token = dict(urlparse.parse_qsl(content))
+
+            session.oauth_token = request_token['oauth_token']
+            session.oauth_token_secret = request_token['oauth_token_secret']
+
+            raise web.seeother('{0}?oauth_token={1}'.format(authorize_url,
+                                                      request_token['oauth_token']))
+
+
+class logout:
+    def GET(self):
+        session.kill()
+        raise web.seeother('/')
 
 
 class pdf:
@@ -55,6 +115,8 @@ class pdf:
 
 
 app = web.application(urls, globals())
+session = web.session.Session(app, web.session.DiskStore('sessions'),
+                              initializer={'user': None})
 
 
 if __name__ == '__main__':
